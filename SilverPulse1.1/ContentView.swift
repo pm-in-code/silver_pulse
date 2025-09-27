147263440f437ca29165c164f1abf80d4889989d
Your Name <your.email@example.com>
2025-09-27 20:26:39 +0200

    Initial Commit

---
+++ SilverPulse1.1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SilverPulse1.1
+//
+//  Created by Dmitry on 27/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SilverPulse1.1/SilverPulse1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SilverPulse1_1App.swift
+//  SilverPulse1.1
+//
+//  Created by Dmitry on 27/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SilverPulse1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
